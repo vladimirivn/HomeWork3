87f4d7321f96b4b96304bd4161a9901d887c61d5
SIDORYCH <vladimiriv@hotmail.com>
2022-12-01 02:10:41 +0300

    ДЗ Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Домашнее задание");
+
+        System.out.println("Задача 1");
+
+        byte bVar = 111;
+        short shVar = 3333;
+        int iVar = 1234567890;
+        long lVar = 45L;
+        float fVar = 0.234f;
+        double dVar = 13.12;
+        char chVar = 42;
+        boolean isVar = shVar < 5000;
+        System.out.println("Значение переменной bVar с типом byte равно: " + bVar);
+        System.out.println("Значение переменной shVar с типом byte равно: " + shVar);
+        System.out.println("Значение переменной iVar с типом byte равно: " + iVar);
+        System.out.println("Значение переменной lVar с типом byte равно: " + lVar);
+        System.out.println("Значение переменной fVar с типом byte равно: " + fVar);
+        System.out.println("Значение переменной dVar с типом byte равно: " + dVar);
+        System.out.println("Значение переменной chVar с типом byte равно: " + chVar);
+        System.out.println("Значение переменной isVar с типом byte равно: " + isVar);
     }
 }
